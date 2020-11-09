cacf8ffb32cc452ed598060292018d2b0e22af2d
OnlyVoidd <mezzediting@gmail.com>
2020-11-09 09:28:20 +0100

    Maj

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,5 +1,22 @@
 package Server;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainServer {
+public static void main(String[] args) {
+	try {
+		ServerSocket ecoute = new ServerSocket(1500);
+		System.out.println("Serveur lancé!");
+		int id=0;	
+		while(true) {
+		Socket client1 = ecoute.accept();
+		Socket client2 = ecoute.accept();
+		new ThreadChat(id,client1,client2).start();
+		id++;
+		}
+		} catch(Exception e) {
+		// Traitement d'erreur
+		}
 
+}
 }
--- src/Server/ThreadChat.java
+++ src/Server/ThreadChat.java
@@ -1,5 +1,46 @@
 package Server;
 
-public class ThreadChat {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ThreadChat extends Thread {
+	int id;
+	BufferedReader in1,in2;
+	PrintWriter out1,out2;
+	static int nbid=0;
+	
+	public void ecrire(String msg) {
+		out1.println(msg);
+		out2.println(msg);
+	}
+	
+	
+	public ThreadChat(int id, Socket client1, Socket client2) {
+		try {
+			this.id = id;
+			nbid++;
+			in1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
+			out1 = new PrintWriter(client1.getOutputStream(), true);
+			ecrire("Id de la partie="+id+"\n");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	
+	public void run() {
+		try {
+			while (true) {
+				String message1=in1.readLine();
+				out2.println(message1);
+				String message2=in1.readLine();
+				out2.println(message2);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
