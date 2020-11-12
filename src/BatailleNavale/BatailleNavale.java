f3451b68e2b7b2426f424b8c50167d59c512031b
OnlyVoidd <mezzediting@gmail.com>
2020-11-12 01:25:16 +0100

    ajout de la fonction ajouterVehicule()

--- src/BatailleNavale/BatailleNavale.java
+++ src/BatailleNavale/BatailleNavale.java
@@ -1,11 +1,22 @@
 package BatailleNavale;
 
 public class BatailleNavale {
-	public static void main(String[] args) {
+	public static void main /*run*/(String[] args){
 		Tableau test = new Tableau();
+		System.out.println("Initialisation");
 		System.out.println(test.afficher());
 		System.out.println("Ajout de la case B2");
 		test.ajouter("B2");
+		System.out.println(test.afficher());
+		System.out.println("Suppression de la case B2");
+		test.supprimer("B2");
+		System.out.println(test.afficher());
+		test.ajouter("B2");
+		int a = test.attaque("B2");
+		int b = test.attaque("A1");
+		System.out.println("Attaque B2 : " + a + "\nAttaque A1 : " + b);
+		System.out.println("Ajout d'un véhicule de C3 à C5");
+		test.ajouterVehicule("C3", "C5");
 		System.out.println(test.afficher());
 	}
 }
--- src/BatailleNavale/Tableau.java
+++ src/BatailleNavale/Tableau.java
@@ -15,17 +15,29 @@ public class Tableau {
 		
 		
 		public String afficher() {
-			String sortie = "";
+			String sortie = " ";
+			
+			
+			for(int i = 1;i<11;i++) {
+				sortie += " " + (i);
+			}
+			sortie += "\n";
+			
+			
 			for(int i = 0; i<10;i++) {
+				sortie += " ";
 				for(int j = 0; j<10;j++) {
 					sortie += "--";
 				}
-				sortie += "-\n";
+				sortie += "-\n" + conv[i];
 				for(int j = 0; j<10;j++) {
 					sortie += "|" + tab[i][j];
 				}
 				sortie += "|\n";
 			}
+			
+			
+			sortie += " ";
 			for(int j = 0; j<10;j++) {
 				sortie += "--";
 			}
@@ -33,7 +45,8 @@ public class Tableau {
 			
 			return sortie;
 		}
-		
+
+
 		public int[] convert(String coord) {
 			int[] s = new int[2];
 			for(int i = 0;i<10;i++) {
@@ -44,8 +57,12 @@ public class Tableau {
 		}
 		
 		public void ajouter(String coord) {
-			int[] coordonnes =convert(coord); 
+			int[] coordonnes = convert(coord); 
 			tab[coordonnes[0]][coordonnes[1]] = 1;
+		}
+		
+		public void ajouterBrut (int[] coord) {
+			tab[coord[0]][coord[1]] = 1;
 		}
 		
 		
@@ -66,5 +83,30 @@ public class Tableau {
 			}
 		}
 		
+		
+		public void ajouterVehicule(String coord1,String coord2) {
+			int[] a1 = convert(coord1);
+			int[] a2 = convert(coord2);
+			int[] remplissage = {0,0};
+			boolean remplissage_fini = false;
+			int k = 0;
+			while(!remplissage_fini) {
+				if(a1[0] == a2[0] && a1[1] != a2[1]) {
+					remplissage[0] = a1[0];
+					remplissage[1] = a1[1] + k;
+					ajouterBrut(remplissage);
+					k++;
+					if (a1[1] + k > a2[1]) remplissage_fini = true;
+				}
+				
+				else if (a1[0] != a2[0] && a1[1] == a2[1]) {
+					remplissage[1] = a1[1];
+					remplissage[0] = a1[0] + k;
+					ajouterBrut(remplissage);
+					k++;
+					if (a1[0] + k > a2[0]) remplissage_fini = true;
+				}
+			}
+		}
 		
 }
